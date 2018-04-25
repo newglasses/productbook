fa7a599c2e5d2fe34d37aea415cfe9d263275d97
Anne mcauley <A.mcauley@kainos.com>
2018-04-25 16:13:53 +0100

    refactor to start api using script

--- server.js
+++ server.js
@@ -23,9 +23,5 @@ app.use((err, req, res, next) => {
     });
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, ()=> console.log(`listening on port ${port}`));
-
 module.exports = app;
 
